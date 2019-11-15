646c470aaa093072cd5cdbdc02c737fdbb59ffb2
Tom-Reilly <thomas.reilly@gov.scot>
2019-11-15 10:13:39 +0000

    Update DATRASFunctions.R

--- R/DATRASFunctions.R
+++ R/DATRASFunctions.R
@@ -6,15 +6,15 @@ getCruiseInfo <- function(CruiseSeries) {
   temp <- sqlFetch(channel, "dbo.tblSeriesStratum")
 
   SurveyQuarter <- as.numeric(gsub("[^0-9]", "", CruiseSeries))
-  if(substr(CruiseSeries,1,6) == "WCIBTS" & SurveyQuarter == 1) {SurveyAreaType <- 21
-  } else {
-    if(substr(CruiseSeries,1,6) == "WCIBTS" & SurveyQuarter == 4) {SurveyAreaType <- 22
-    } else {
-      SurveyAreaType <- 0
-    }
-  }
+  #if(substr(CruiseSeries,1,6) == "WCIBTS" & SurveyQuarter == 1) {SurveyAreaType <- 21
+  #} else {
+  #  if(substr(CruiseSeries,1,6) == "WCIBTS" & SurveyQuarter == 4) {SurveyAreaType <- 22
+  #  } else {
+  #    SurveyAreaType <- 0
+  #  }
+  #}
 
-  #SurveyAreaType <- 0
+  SurveyAreaType <- 0
 
   if(any(grepl(CruiseSeries, temp[["fldSeriesName"]]))) {HHStratum <- 1} else {HHStratum <- 0}
 
@@ -25,4 +25,4 @@ getCruiseInfo <- function(CruiseSeries) {
   return(cruiseInfo)
 }
 
-# 02/11/2017 - Changed the survey area type to be 0 for all cruises as the area code is now always set to be stat sq ratehr than stratum
+# 02/11/2017 - Changed the survey area type to be 0 for all cruises as the area code is now always set to be stat sq rather than stratum
