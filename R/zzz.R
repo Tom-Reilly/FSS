b9ff50eeb328220bc9280bff35f95fb80ba9b18d
Tom-Reilly <thomas.reilly@gov.scot>
2020-02-24 11:51:08 +0000

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,5 @@
 .onLoad <- function(libname = find.package("FSS"), pkgname = "FSS") {
-  ODBCConnect("FSS","OSE")
+  source("//SOSE0014F/Code/FSS_R_Package/config.R")
+  ODBCConnect(name, user, password)
+  rm(list=c("name","user","password"), envir = .GlobalEnv)
 }
