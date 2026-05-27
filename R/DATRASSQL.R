77222decaa9eb002d8b5828e72bfc454877d8a00
Tom-Reilly <thomas.reilly@gov.scot>
2026-05-27 12:48:14 +0100

    Update DATRASSQL.R

--- R/DATRASSQL.R
+++ R/DATRASSQL.R
@@ -124,8 +124,8 @@ caCore_qry <- function(cruiseCode, uniqueGear) {
     		COUNT(a.fldInternalBiologicalSampleID) AS CaNoAtLen  
 		FROM dbo.tblDataBiologicalSamples a INNER JOIN dbo.tblDataGearDeployments b ON(a.fldCruiseName=b.fldCruisename AND a.fldCruiseStationNumber=b.fldCruiseStationNumber)
 		JOIN dbo.tblReferenceMainGearCodes ON a.fldGearCode = dbo.tblReferenceMainGearCodes.fldGearCode
-  		WHERE a.fldCruiseName='", cruiseCode, "' AND dbo.tblReferenceMainGearCodes.fldDATRASCode='", uniqueGear, "' AND a.fldResult1 is not null AND b.fldValidityCode='V' GROUP BY a.fldCruiseStationNumber, a.fldGearCode, dbo.tblReferenceMainGearCodes.fldDATRASCode, a.fldMainSpeciesCode,  a.fldFishLength, a.fldFishSex, a.fldFishMaturity, a.fldResult1; ", sep = "") 
-  ca_sql <- gsub('\n','',ca_sql)	
+  		WHERE a.fldCruiseName='", cruiseCode, "' AND dbo.tblReferenceMainGearCodes.fldDATRASCode='", uniqueGear, "' AND b.fldValidityCode='V' GROUP BY a.fldCruiseStationNumber, a.fldGearCode, dbo.tblReferenceMainGearCodes.fldDATRASCode, a.fldMainSpeciesCode,  a.fldFishLength, a.fldFishSex, a.fldFishMaturity, a.fldResult1; ", sep = "") 
+  ca_sql <- gsub('\n','',ca_sql)	# AND a.fldResult1 is not null 
   caCore_qry <- gsub('\t','',ca_sql)  
   
 }
